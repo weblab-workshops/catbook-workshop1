9b548c3e9c95a7ab12c740c7de27ed6296c241d8
Danny Tang <data1013@mit.edu>
2018-01-08 17:36:04 -0500

    adding basic skeleton for feed.js

---
+++ js/feed.js
@@ -0,0 +1,16 @@
+const API_ENDPOINT = 'https://mit-wdc-workshop-1.herokuapp.com';
+
+// GET /api/stories fetches all stories
+// GET /api/comment fetches all comments for a story, given the story's id (passed as the 'parent' parameter)
+
+// Creates an html block for a story
+function storyDOMObject(storyJSON) {
+}
+
+// Creates a comment block for a story
+function commentDOMObject(commentJSON) {
+}
+
+// Makes API requests and calls helper functions
+function renderStories() {
+}
