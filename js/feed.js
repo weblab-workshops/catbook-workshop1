790c08d154d9dcec8fb3c9c34ed6cad6827a5406
Danny Tang <data1013@mit.edu>
2018-01-09 20:21:35 -0500

    branch ex2_part1

--- js/feed.js
+++ js/feed.js
@@ -5,6 +5,34 @@ const API_ENDPOINT_START = 'https://mit-wdc-workshop-1.herokuapp.com';
 
 // Creates an html block for a story
 function storyDOMObject(storyJSON) {
+  const card = document.createElement('div');
+  card.setAttribute('id', storyJSON._id);
+  card.className = 'story card';
+
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+  card.appendChild(cardBody);
+
+  const creatorSpan = document.createElement('a');
+  creatorSpan.className = 'story-creator card-title';
+  creatorSpan.innerHTML = storyJSON.creator_name;
+  cardBody.appendChild(creatorSpan);
+
+  const contentSpan = document.createElement('p');
+  contentSpan.className = 'story-content card-text';
+  contentSpan.innerHTML = storyJSON.content;
+  cardBody.appendChild(contentSpan);
+
+  const cardFooter = document.createElement('div');
+  cardFooter.className = 'card-footer';
+  card.appendChild(cardFooter);
+
+  const commentsDiv = document.createElement('div');
+  commentsDiv.setAttribute('id', storyJSON._id + '-comments');
+  commentsDiv.className = 'story-comments';
+  cardFooter.appendChild(commentsDiv);
+
+  return card;
 }
 
 // Creates a comment block for a story
@@ -13,4 +41,11 @@ function commentDOMObject(commentJSON) {
 
 // Makes API requests and calls helper functions
 function renderStories() {
+  const storiesDiv = document.getElementById('stories');
+  get(API_ENDPOINT_START + '/api/stories', {}, function(storiesArr) {
+    for (let i = 0; i < storiesArr.length; i++) {
+      const currentStory = storiesArr[i];
+      storiesDiv.prepend(storyDOMObject(currentStory));
+    }
+  });
 }
